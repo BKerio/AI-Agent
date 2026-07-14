39759bc59e1348e820566caa9e1821d3116349c7
BKerio <briankerio48@gmail.com>
2026-07-14 22:13:27 +0300

    new changes

--- src/whatsapp/client.py
+++ src/whatsapp/client.py
@@ -1,7 +1,7 @@
 import httpx
 from src.config import WHATSAPP_PHONE_NUMBER_ID, WHATSAPP_API_TOKEN
 
-API_BASE = f"https://graph.jjfacebook.com/v17.0/{WHATSAPP_PHONE_NUMBER_ID}"
+API_BASE = f"https://graph.facebook.com/v17.0/{WHATSAPP_PHONE_NUMBER_ID}"
 HEADERS = {
     "Authorization": f"Bearer {WHATSAPP_API_TOKEN}",
     "Content-Type": "application/json"
